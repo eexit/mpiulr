699fbfef1e2989551133f9e4a269f0ab91e8d647
Joris Berthelot <admin+github@eexit.net>
2011-03-01 12:31:09 +0000

    Service tests files

---
+++ tests/fr/univ/lr/mpi/services/impl/AnsweringMachineMessageTest.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package fr.univ.lr.mpi.services.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * MPI_PROJECT/fr.univ.lr.mpi.services.impl/AnsweringMachineMessageTest.java
+ *
+ * @author Joris Berthelot <joris.berthelot@gmail.com>
+ * @date Mar 1, 2011
+ *
+ */
+public class AnsweringMachineMessageTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link fr.univ.lr.mpi.services.impl.AnsweringMachineMessage#AnsweringMachineMessage(java.util.Date, java.lang.String, java.lang.Object)}.
+	 */
+	@Test
+	public void testAnsweringMachineMessage() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link fr.univ.lr.mpi.services.impl.AnsweringMachineMessage#getPostDate()}.
+	 */
+	@Test
+	public void testGetPostDate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link fr.univ.lr.mpi.services.impl.AnsweringMachineMessage#getPosterPhoneNumber()}.
+	 */
+	@Test
+	public void testGetPosterPhoneNumber() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link fr.univ.lr.mpi.services.impl.AnsweringMachineMessage#getMessage()}.
+	 */
+	@Test
+	public void testGetMessage() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ tests/fr/univ/lr/mpi/services/impl/DirectoryServiceTest.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package fr.univ.lr.mpi.services.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * MPI_PROJECT/fr.univ.lr.mpi.services.impl/DirectoryServiceTest.java
+ *
+ * @author Joris Berthelot <joris.berthelot@gmail.com>
+ * @date Mar 1, 2011
+ *
+ */
+public class DirectoryServiceTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link fr.univ.lr.mpi.services.impl.DirectoryService#DirectoryService()}.
+	 */
+	@Test
+	public void testDirectoryService() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link fr.univ.lr.mpi.services.impl.DirectoryService#addPhoneNumber(java.lang.String)}.
+	 */
+	@Test
+	public void testAddPhoneNumber() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link fr.univ.lr.mpi.services.impl.DirectoryService#isExisting(java.lang.String)}.
+	 */
+	@Test
+	public void testIsExisting() {
+		fail("Not yet implemented");
+	}
+
+}
