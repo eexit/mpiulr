880b3386cb26d82522c7a6bec5a28db34bb20cec
Joris Berthelot <admin+github@eexit.net>
2011-03-01 13:37:52 +0000

    new interface/MessageHandler

--- src/fr/univ/lr/mpi/handlers/MessageHandler.java
+++ src/fr/univ/lr/mpi/handlers/MessageHandler.java
@@ -1,5 +1,11 @@
 package fr.univ.lr.mpi.handlers;
 
 public interface MessageHandler {
-
+	
+	/**
+	 * Message receiver from commutator container
+	 * 
+	 * @author Joris Berthelot <joris.berthelot@gmail.com>
+	 */
+	public void receiveMessage();
 }
