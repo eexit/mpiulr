943049c781dc7e6f73193d666e9e2ac936c17be9
joris.berthelot@gmail.com <joris.berthelot@gmail.com@f8d45b5d-80d6-be73-f0dd-ddb87d190268>
2011-03-01 01:33:00 +0000

    Adds test package + empty test classes

---
+++ trunk/tests/fr/univ/lr/mpi/AnsweringServiceTest.java
@@ -0,0 +1,12 @@
+package fr.univ.lr.mpi;
+
+
+import org.junit.Before;
+
+public class AnsweringServiceTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+}
---
+++ trunk/tests/fr/univ/lr/mpi/BillingServiceTest.java
@@ -0,0 +1,12 @@
+package fr.univ.lr.mpi;
+
+
+import org.junit.Before;
+
+public class BillingServiceTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+}
