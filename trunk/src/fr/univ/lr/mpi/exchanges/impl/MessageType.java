ad57e0ea1c286871b051a15aa3f46e7a1e63e393
elian.oriou <elian.oriou@f8d45b5d-80d6-be73-f0dd-ddb87d190268>
2011-03-01 15:38:02 +0000

    Commit of the MessageType and EventType enumerations

--- trunk/src/fr/univ/lr/mpi/exchanges/impl/MessageType.java
+++ trunk/src/fr/univ/lr/mpi/exchanges/impl/MessageType.java
@@ -1,5 +1,49 @@
 package fr.univ.lr.mpi.exchanges.impl;
 
+/**
+ * An enumerated type. Contains all messages exchanged within our system.
+ * 
+ * @author Elian ORIOU <elian.oriou@gmail.com>
+ * 
+ */
+
 public enum MessageType {
 
+	/**
+	 * When the caller picks up his phone
+	 */
+	PICKUP,
+	/**
+	 * When the AutoCommutator returns the tone (the connection between the
+	 * caller and the AutoCommutator) to the caller
+	 */
+	BACKTONE,
+	/**
+	 * When the caller compose a phone number
+	 */
+	NUMEROTATION,
+	/**
+	 * When the AutoCommutator searches for a recipient and returns this signal
+	 * to the caller
+	 */
+	SEARCH,
+	/**
+	 * When the AutoCommutator send a signal to the recipient (only if the line
+	 * is free)
+	 */
+	RING,
+	/**
+	 * When the recipient phone rings the AutoCommutator send an ECHO signal to
+	 * the caller
+	 */
+	ECHO,
+	/**
+	 * When the connection is established, the caller and the recipient can
+	 * exchange voice data
+	 */
+	VOICE_EXCHANGE,
+	/**
+	 * When the caller OR the recipient hang up their phones
+	 */
+	HANGUP;
 }
