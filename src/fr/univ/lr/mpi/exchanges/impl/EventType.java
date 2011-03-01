79b730fe1e5476f784fcf21c2cc5f95ae252f6a8
Elian ORIOU <eoriou@gmail.com>
2011-03-01 16:14:42 +0000

    Changes on Event & Message Management (on AutoCommutator) Adding of an Event/Message Attributes Name Mapper (ExchangeAttributeName) Completing EventType enumerated type

--- src/fr/univ/lr/mpi/exchanges/impl/EventType.java
+++ src/fr/univ/lr/mpi/exchanges/impl/EventType.java
@@ -10,15 +10,23 @@ package fr.univ.lr.mpi.exchanges.impl;
 public enum EventType {
 
 	/**
-	 * 
+	 * When a line is created
+	 */
+	LINE_CREATION,
+	/**
+	 * When a line is deleted
+	 */
+	LINE_DELETION,
+	/**
+	 * When a connection is established between caller & recipient
 	 */
 	CONNECTION_ESTABLISHED,
 	/**
-	 * 
+	 * When a connection is closed between caller & recipient
 	 */
 	CONNECTION_CLOSED,
 	/**
-	 * 
+	 * When a caller requests a recipient number
 	 */
 	PHONE_NUMBER_REQUEST;
 }
---
+++ src/fr/univ/lr/mpi/exchanges/impl/ExchangeAttributeNames.java
@@ -0,0 +1,42 @@
+package fr.univ.lr.mpi.exchanges.impl;
+
+/**
+ * Class that contains only constants. These constants concerns attribute name
+ * into Event and Message Exchange Units.
+ * 
+ * @author Elian ORIOU <elian.oriou@gmail.com>
+ * 
+ */
+
+public class ExchangeAttributeNames {
+
+	/**
+	 * 
+	 */
+	public static final String CALLER_PHONE_NUMER = "CALL_PHONE_NUMBER";
+
+	/**
+	 * 
+	 */
+	public static final String RECIPIENT_PHONE_NUMER = "RECIPIENT_PHONE_NUMBER";
+
+	/**
+	 * 
+	 */
+	public static final String CONNECTION_DURATION = "CONNECTION_DURATION";
+
+	/**
+	 * 
+	 */
+	public static final String EXISTS = "EXISTS";
+
+	/**
+	 * 
+	 */
+	public static final String MESSAGE = "MESSAGE";
+
+	/**
+	 * 
+	 */
+	public static final String DATE = "DATE";
+}
