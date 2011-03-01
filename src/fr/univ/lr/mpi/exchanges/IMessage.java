aa3962a1a06ecacab25225471e8017abaecb8667
Joris Berthelot <admin+github@eexit.net>
2011-03-01 13:57:58 +0000

    update comments

--- src/fr/univ/lr/mpi/exchanges/IMessage.java
+++ src/fr/univ/lr/mpi/exchanges/IMessage.java
@@ -2,10 +2,32 @@ package fr.univ.lr.mpi.exchanges;
 
 import fr.univ.lr.mpi.exchanges.impl.MessageType;
 
+/**
+ * @author Tony FAUCHER
+ */
 public interface IMessage {
-
+	/**
+	 * Gets the message type
+	 * 
+	 * @author Tony FAUCHER
+	 * @return
+	 */
 	public MessageType getMessageType();
+	
+	/**
+	 * Gets the caller phone number
+	 * 
+	 * @author Tony FAUCHER
+	 * @return
+	 */
 	public String getCallerPhoneNumber();
+	
+	/**
+	 * Gets the recipient phone number
+	 * 
+	 * @author Tony FAUCHER
+	 * @return
+	 */
 	public String getRecipientPhoneNumber();
 	
 }
