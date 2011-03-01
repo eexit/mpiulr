025ad1dd071d2dd4fa7c2fb42c2d1a2e78b57acb
Joris Berthelot <admin+github@eexit.net>
2011-03-01 13:46:35 +0000

    + LineTest = IConnection = Connection = ILine = Line - pref file

--- src/fr/univ/lr/mpi/commutator/impl/Connection.java
+++ src/fr/univ/lr/mpi/commutator/impl/Connection.java
@@ -1,7 +1,83 @@
 package fr.univ.lr.mpi.commutator.impl;
 
+import java.sql.Date;
+import java.util.Timer;
 import fr.univ.lr.mpi.commutator.IConnection;
+import fr.univ.lr.mpi.exchanges.IMessage;
+import fr.univ.lr.mpi.exchanges.impl.Message;
+import fr.univ.lr.mpi.lines.ILine;
 
-public class Connection implements IConnection{
-
+/**
+ * 
+ * MPI_PROJECT/fr.univ.lr.mpi.commutator.impl/Connection.java
+ *
+ * @author Joris Berthelot <joris.berthelot@gmail.com>
+ * @date Mar 1, 2011
+ *
+ */
+public class Connection implements IConnection {
+	
+	/**
+	 * Caller line container
+	 * 
+	 * @author Joris Berthelot <joris.berthelot@gmail.com>
+	 */
+	private ILine caller;
+	
+	/**
+	 * Recipient line container
+	 * 
+	 * @author Joris Berthelot <joris.berthelot@gmail.com>
+	 */
+	private ILine recipient;
+	
+	/**
+	 * Connection start time
+	 * 
+	 * @author Joris Berthelot <joris.berthelot@gmail.com>
+	 */
+	private Date startTime;
+	
+	/**
+	 * Connection end time
+	 * 
+	 * @author Joris Berthelot <joris.berthelot@gmail.com>
+	 */
+	private Date endTime;
+	
+	/**
+	 * Connection timer which will be launch when one of the
+	 * connection lines hang up. When to timer reaches it limit,
+	 * the connection is destroyed.
+	 * 
+	 * @author Joris Berthelot <joris.berthelot@gmail.com>
+	 */
+	private Timer timer;
+	
+	/**
+	 * Commutator container
+	 * 
+	 * @author Joris Berthelot <joris.berthelot@gmail.com>
+	 */
+	private AutoCommutator commutator;
+	
+	/**
+	 * Message forwarding between the two connected lines
+	 * 
+	 * @author Joris Berthelot <joris.berthelot@gmail.com>
+	 * @param message
+	 */
+	public void forwardMessage(Message message) {
+		
+	}
+	
+	/**
+	 * Message receiver of commutator container
+	 * 
+	 * @author Joris Berthelot <joris.berthelot@gmail.com>
+	 */
+	public void receiveMessage(IMessage message) {
+		// TODO Auto-generated method stub
+		
+	}
 }
---
+++ tests/fr/univ/lr/mpi/lines/LineTest.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ *
+ * @author Joris Berthelot <joris.berthelot@gmail.com>
+ */
+package fr.univ.lr.mpi.lines;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * MPI_PROJECT/fr.univ.lr.mpi.lines/LineTest.java
+ *
+ * @author Joris Berthelot <joris.berthelot@gmail.com>
+ * @date Mar 1, 2011
+ *
+ */
+public class LineTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link fr.univ.lr.mpi.lines.Line#getPhoneNumber()}.
+	 */
+	@Test
+	public void testGetPhoneNumber() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link fr.univ.lr.mpi.lines.Line#setConnection(fr.univ.lr.mpi.commutator.IConnection)}.
+	 */
+	@Test
+	public void testSetConnection() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link fr.univ.lr.mpi.lines.Line#getLineState()}.
+	 */
+	@Test
+	public void testGetLineState() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link fr.univ.lr.mpi.lines.Line#setState(fr.univ.lr.mpi.lines.LineState)}.
+	 */
+	@Test
+	public void testSetState() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link fr.univ.lr.mpi.lines.Line#receiveMessage(fr.univ.lr.mpi.exchanges.IMessage)}.
+	 */
+	@Test
+	public void testReceiveMessage() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link fr.univ.lr.mpi.lines.Line#pickUp()}.
+	 */
+	@Test
+	public void testPickUp() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link fr.univ.lr.mpi.lines.Line#hangUp()}.
+	 */
+	@Test
+	public void testHangUp() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link fr.univ.lr.mpi.lines.Line#dialTo(java.lang.String)}.
+	 */
+	@Test
+	public void testDialTo() {
+		fail("Not yet implemented");
+	}
+
+}
