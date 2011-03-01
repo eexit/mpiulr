c9ea0eaf12cddd337309a838ecb3148342b1d669
joris.berthelot@gmail.com <joris.berthelot@gmail.com@f8d45b5d-80d6-be73-f0dd-ddb87d190268>
2011-03-01 14:06:29 +0000

    new ConnectionTest updated comments IEvent/IMessage

--- trunk/src/fr/univ/lr/mpi/exchanges/IEvent.java
+++ trunk/src/fr/univ/lr/mpi/exchanges/IEvent.java
@@ -1,13 +1,44 @@
 package fr.univ.lr.mpi.exchanges;
 
 import java.util.Map;
-
 import fr.univ.lr.mpi.exchanges.impl.EventType;
 
+/**
+ * @author Tony FAUCHER
+ */
 public interface IEvent {
 	
+	/**
+	 * Gets the event type
+	 * 
+	 * @author Tony FAUCHER
+	 * @return
+	 */
 	public EventType getEventType();
+	
+	/**
+	 * Gets the event attribute value
+	 * 
+	 * @author Tony FAUCHER
+	 * @param attributeName
+	 * @return
+	 */
 	public String getAttributeValue(String attributeName);
+	
+	/**
+	 * Gets the event attributes
+	 * 
+	 * @author Tony FAUCHER
+	 * @return
+	 */
 	public Map<String, String> getAttributes();
+	
+	/**
+	 * Appends a new attribute to an event
+	 * 
+	 * @author Tony FAUCHER
+	 * @param attributeNames
+	 * @param attributeValue
+	 */
 	public void addAttributes(String attributeNames, String attributeValue);
 }
