47683bf3ca7a015873ff447e6a93f68e0cc84cc9
elian.oriou <elian.oriou@f8d45b5d-80d6-be73-f0dd-ddb87d190268>
2011-03-01 21:59:22 +0000

    A small simulation is borned (to be alive) !

--- trunk/simulation/fr/univ/lr/mpi/simulation/Simulation.java
+++ trunk/simulation/fr/univ/lr/mpi/simulation/Simulation.java
@@ -14,17 +14,30 @@ public class Simulation {
 	public static void main(String[] args) {
 		AutoCommutator commutator = AutoCommutator.getInstance();
 
-		Line l1 = new Line("0102030405");
+		Line l1 = new Line("0102030105");
 		Line l2 = new Line("0203040506");
 		Line l3 = new Line("0304050607");
-		Line l4 = new Line("0506070809");
+		Line l4 = new Line("0405060708");
 
 		commutator.registerLine(l1);
 		commutator.registerLine(l2);
 		commutator.registerLine(l3);
 		commutator.registerLine(l4);
-		
+
 		l1.pickUp();
 		l2.pickUp();
+
+		System.out.println("Actives Connections : "
+				+ commutator.getActiveConnections());
+	}
+
+	public static String generatePhoneNumber(int length) {
+		String returns = "05";
+		for (int i = 0; i < length - 2; i++) {
+			int j = (int) Math.round(Math.random() * 10);
+			returns += j;
+		}
+		System.out.println(returns);
+		return returns;
 	}
 }
