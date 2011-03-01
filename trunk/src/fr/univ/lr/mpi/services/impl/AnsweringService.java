537f8dc40b8309ee6aacb0a2886940f3b35f20ea
faucher.tony85 <faucher.tony85@f8d45b5d-80d6-be73-f0dd-ddb87d190268>
2011-03-01 10:36:41 +0000

    Services Creation

--- trunk/src/fr/univ/lr/mpi/services/impl/AnsweringService.java
+++ trunk/src/fr/univ/lr/mpi/services/impl/AnsweringService.java
@@ -1,8 +1,46 @@
 package fr.univ.lr.mpi.services.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import fr.univ.lr.mpi.services.IService;
 
+/**
+ * Answering Service
+ * 
+ * @author FAUCHER Tony
+ */
 
 public class AnsweringService implements IService {
+
+	// attributes
+	private List<AnsweringMachineMessage> messages;
+
+	// constructor
+	/**
+	 * @author FAUCHER Tony constructor of an Answering Service
+	 */
+	public AnsweringService() {
+		this.messages = new ArrayList<AnsweringMachineMessage>();
+	}
+
+	// méthodes
+
+	/**
+	 * @author FAUCHER Tony Add an Answering Machine Message
+	 * @param AnsweringMachineMessage
+	 * 
+	 */
+	public void postMessage(AnsweringMachineMessage asm) {
+		this.messages.add(asm);
+	}
+
+	/**
+	 * @author FAUCHER Tony return the list of the answering machine message
+	 * @return message list
+	 */
+	public List<AnsweringMachineMessage> getMessages() {
+		return this.messages;
+	}
 
 }
--- trunk/src/fr/univ/lr/mpi/services/impl/DirectoryService.java
+++ trunk/src/fr/univ/lr/mpi/services/impl/DirectoryService.java
@@ -1,5 +1,49 @@
 package fr.univ.lr.mpi.services.impl;
 
-public class DirectoryService {
+import java.util.ArrayList;
+import java.util.List;
 
+import fr.univ.lr.mpi.services.IService;
+
+/**
+ * 
+ * @author FAUCHER Tony
+ * 
+ */
+public class DirectoryService implements IService {
+
+	// attribute
+	private List<String> directory;
+
+	/**
+	 * Constructor
+	 * 
+	 * @author FAUCHER Tony
+	 */
+	public DirectoryService() {
+		this.directory = new ArrayList<String>();
+	}
+
+	/**
+	 * @author FAUCHER Tony Add a phone number in the directory
+	 * @param phoneNumber
+	 */
+	public void addPhoneNumber(String phoneNumber) {
+		this.directory.add(phoneNumber);
+	}
+
+	/**
+	 * @author FAUCHER Tony
+	 * @param phoneNumber
+	 * @return if the phone number is in the directory or not
+	 */
+	public boolean isExisting(String phoneNumber) {
+		boolean exist = false;
+		for (int i = 0; i < this.directory.size() && !exist; i++) {
+			if (phoneNumber.equals(this.directory.get(i))) {
+				exist = true;
+			}
+		}
+		return exist;
+	}
 }
