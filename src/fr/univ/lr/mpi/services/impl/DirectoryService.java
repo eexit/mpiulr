7d9c0f5c65521eae55e393a721f8f5e4993d9b50
Elian ORIOU <eoriou@gmail.com>
2011-03-02 17:36:09 +0000

    Changes on Services event management (managed as Thread by Event Stack poping)

--- src/fr/univ/lr/mpi/services/impl/DirectoryService.java
+++ src/fr/univ/lr/mpi/services/impl/DirectoryService.java
@@ -2,6 +2,7 @@ package fr.univ.lr.mpi.services.impl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import fr.univ.lr.mpi.commutator.impl.AutoCommutator;
 import fr.univ.lr.mpi.exchanges.IEvent;
@@ -16,12 +17,14 @@ import fr.univ.lr.mpi.services.IService;
  * @author FAUCHER Tony <faucher.tony85@gmail.com>
  * 
  */
-public class DirectoryService implements IService {
+public class DirectoryService extends Thread implements IService {
 
 	/**
-	 * List of the differents lines
+	 * List of the different lines
 	 */
 	private List<String> directory;
+
+	private Stack<IEvent> eventStack;
 
 	/**
 	 * Constructor
@@ -29,6 +32,78 @@ public class DirectoryService implements IService {
 	 */
 	public DirectoryService() {
 		this.directory = new ArrayList<String>();
+		this.eventStack = new Stack<IEvent>();
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			String phoneNumber;
+			if (eventStack.isEmpty()) {
+				try {
+					wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			IEvent event = eventStack.pop();
+			switch (event.getEventType()) {
+
+			/**
+			 * Case of line creation
+			 */
+			case LINE_CREATION:
+				// Get the phone number
+				phoneNumber = event
+						.getAttributeValue(ExchangeAttributeNames.CALLER_PHONE_NUMBER);
+				this.directory.add(phoneNumber);
+				break;
+
+			/**
+			 * Case of line deletion
+			 */
+			case LINE_DELETION:
+				// Get the phone number
+				phoneNumber = event
+						.getAttributeValue(ExchangeAttributeNames.CALLER_PHONE_NUMBER);
+				this.directory.remove(phoneNumber);
+				break;
+
+			/**
+			 * We need to know if the number is in the directory
+			 */
+			case PHONE_NUMBER_REQUEST:
+				// Get the phone number
+				phoneNumber = event
+						.getAttributeValue(ExchangeAttributeNames.RECIPIENT_PHONE_NUMBER);
+
+				// if the phone number is in the directory, we return true, else
+				// we return false
+
+				// Create a new event
+				Event newEvent = new Event(EventType.PHONE_NUMBER_RESPONSE);
+				// add caller phone number
+				newEvent
+						.addAttribute(
+								ExchangeAttributeNames.CALLER_PHONE_NUMBER,
+								event
+										.getAttributeValue(ExchangeAttributeNames.CALLER_PHONE_NUMBER));
+				// add recipient phone number
+				newEvent.addAttribute(
+						ExchangeAttributeNames.RECIPIENT_PHONE_NUMBER,
+						phoneNumber);
+
+				// add boolean
+
+				newEvent.addAttribute(ExchangeAttributeNames.EXISTS, Boolean
+						.toString(this.exist(phoneNumber)));
+
+				// Send to the autocommutator the event
+				AutoCommutator.getInstance().receiveEvent(newEvent);
+
+				break;
+			}
+		}
 	}
 
 	/**
@@ -49,63 +124,8 @@ public class DirectoryService implements IService {
 	}
 
 	@Override
-	public void receiveEvent(IEvent event) {
-		String phoneNumber;
-		switch (event.getEventType()) {
-
-		/**
-		 * Case of line creation
-		 */
-		case LINE_CREATION:
-			// Get the phone number
-			phoneNumber = event
-					.getAttributeValue(ExchangeAttributeNames.CALLER_PHONE_NUMBER);
-			this.directory.add(phoneNumber);
-			break;
-
-		/**
-		 * Case of line deletion
-		 */
-		case LINE_DELETION:
-			// Get the phone number
-			phoneNumber = event
-					.getAttributeValue(ExchangeAttributeNames.CALLER_PHONE_NUMBER);
-			this.directory.remove(phoneNumber);
-			break;
-
-		/**
-		 * We need to know if the number is in the directory
-		 */
-		case PHONE_NUMBER_REQUEST:
-			// Get the phone number
-			phoneNumber = event
-					.getAttributeValue(ExchangeAttributeNames.RECIPIENT_PHONE_NUMBER);
-
-			// if the phone number is in the directory, we return true, else we
-			// return false
-
-			// Create a new event
-			Event newEvent = new Event(EventType.PHONE_NUMBER_RESPONSE);
-			// add caller phone number
-			newEvent
-					.addAttribute(
-							ExchangeAttributeNames.CALLER_PHONE_NUMBER,
-							event
-									.getAttributeValue(ExchangeAttributeNames.CALLER_PHONE_NUMBER));
-			// add recipient phone number
-			newEvent.addAttribute(
-					ExchangeAttributeNames.RECIPIENT_PHONE_NUMBER, phoneNumber);
-
-			// add boolean
-		
-			newEvent.addAttribute(ExchangeAttributeNames.EXISTS, Boolean
-					.toString(this.exist(phoneNumber)));
-
-			// Send to the autocommutator the event
-			AutoCommutator.getInstance().receiveEvent(newEvent);
-
-			break;
-		}
-
+	public synchronized void receiveEvent(IEvent event) {
+		this.eventStack.add(event);
+		this.notify();
 	}
 }
