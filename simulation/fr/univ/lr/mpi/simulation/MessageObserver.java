e9f77f4de3e31d3044243e20e4f1c51a5b282800
faucher.tony85 <faucher.tony85@f8d45b5d-80d6-be73-f0dd-ddb87d190268>
2011-03-03 11:31:25 +0000

    Last version of MessageObserver (error : une fois le thread mis en pause, il ne se relance plus si il y a des evenements nouveaux)

--- simulation/fr/univ/lr/mpi/simulation/MessageObserver.java
+++ simulation/fr/univ/lr/mpi/simulation/MessageObserver.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Queue;
 
+import com.trolltech.qt.gui.QApplication;
+import com.trolltech.qt.gui.QTextBrowser;
+import com.trolltech.qt.gui.QWidget;
+
 import fr.univ.lr.mpi.exchanges.IEvent;
 import fr.univ.lr.mpi.services.IService;
 
@@ -18,26 +22,59 @@ public class MessageObserver extends Thread implements IService {
 
 	public SimulationUI ui;
 	private Queue<IEvent> evenement;
-	
+
+	private QWidget logBrowser;
+	private QTextBrowser logBrowserText;
 
 	public MessageObserver() {
 
 		ui = new SimulationUI();
 		this.evenement = new LinkedList<IEvent>();
-	
+
+		this.logBrowser = new QWidget();
+		this.logBrowserText = new QTextBrowser(logBrowser);
+		this.logBrowserText.setGeometry(0, 0, 800, 255);
+		this.logBrowser.setGeometry(450, 500, 800, 250);
+		
+		try {
+			FileWriter logFile = new FileWriter(
+					"simulation/fr/univ/lr/mpi/log/log.txt");
+			logFile.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 	/**
 	 * If the observer receive a message, he stock it in the queue
 	 */
 	public synchronized void receiveEvent(IEvent event) {
+		System.out.println("log --- "+event);
 		this.evenement.add(event);
+		
+	
+		/**
+		 * Wake up the thread
+		 */
 		this.notify();
 	}
 
+	public QWidget getLogWidget() {
+		return this.logBrowser;
+	}
+
+	/**
+	 * Write in the log file the different event
+	 */
 	public void run() {
 
 		while (true) {
+			/**
+			 * If there is no event in the queue, we pause the Thread
+			 */
 			if (this.evenement.isEmpty()) {
 				synchronized (this) {
 					try {
@@ -48,11 +85,26 @@ public class MessageObserver extends Thread implements IService {
 				}
 			}
 
+			/**
+			 * We write in the file
+			 */
 			try {
-				FileWriter logFile = new FileWriter("simulation/fr/univ/lr/mpi/log/log.txt",true);
+				
+				System.out.println("log ----- ");
 				String log = this.evenement.poll().toString();
-				logFile.write(log+"\r\n");
+				
+				this.logBrowserText.append(log);
+				this.logBrowser.repaint();
+				
+				FileWriter logFile = new FileWriter(
+						"simulation/fr/univ/lr/mpi/log/log.txt", true);
+				
+				
+				
+				logFile.write(log + "\r\n");
 				logFile.close();
+				
+				
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
