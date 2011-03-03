782469a0e337673e7d401232aec83e8811a785e9
faucher.tony85 <faucher.tony85@f8d45b5d-80d6-be73-f0dd-ddb87d190268>
2011-03-03 13:37:20 +0000

    last version of ui simulation/SimulationUI.java

--- simulation/fr/univ/lr/mpi/simulation/SimulationUI.java
+++ simulation/fr/univ/lr/mpi/simulation/SimulationUI.java
@@ -1,22 +1,16 @@
 package fr.univ.lr.mpi.simulation;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import com.trolltech.qt.gui.QApplication;
-import com.trolltech.qt.gui.QTextBrowser;
 import com.trolltech.qt.gui.QWidget;
 
 import fr.univ.lr.mpi.commutator.impl.AutoCommutator;
 import fr.univ.lr.mpi.commutator.impl.Concentrator;
 import fr.univ.lr.mpi.exceptions.LineException;
 import fr.univ.lr.mpi.exceptions.PhoneNumberValidatorException;
-import fr.univ.lr.mpi.exchanges.impl.Event;
-import fr.univ.lr.mpi.exchanges.impl.EventType;
-import fr.univ.lr.mpi.exchanges.impl.ExchangeAttributeNames;
-import fr.univ.lr.mpi.exchanges.impl.Message;
-import fr.univ.lr.mpi.exchanges.impl.MessageType;
+
 import fr.univ.lr.mpi.lines.impl.Line;
 
 /**
@@ -27,6 +21,7 @@ import fr.univ.lr.mpi.lines.impl.Line;
 
 public class SimulationUI {
 
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws LineException,
 			PhoneNumberValidatorException, InterruptedException {
 
@@ -109,73 +104,9 @@ public class SimulationUI {
 		// logBrowser.setParent(windows);
 		// logBrowser.setGeometry(410, windows.height() - 300, 800, 250);
 		// logBrowser.show();
+		
 		windows.show();
-		//
-		// l1.pickUp();
-		// l2.pickUp();
-		//
-		// /* L2 => L4 */
-		// l2.dialTo("0405060708");
-		// Thread.sleep(10000);
-		// commutator.stop();
-		// /* L1 => L3 */
-		// l1.dialTo("0304050607");
-		//
-		// Event e;
-		// /* Directory Service test */
-		// e = new Event(EventType.PHONE_NUMBER_REQUEST);
-		// e
-		// .addAttribute(ExchangeAttributeNames.CALLER_PHONE_NUMBER,
-		// "0102030105");
-		// e.addAttribute(ExchangeAttributeNames.RECIPIENT_PHONE_NUMBER,
-		// "0203040506");
-		// AutoCommutator.getInstance().sendEvent(e);
-		//
-		// /* Billing Service test */
-		// e = new Event(EventType.CONNECTION_CLOSED);
-		// e
-		// .addAttribute(ExchangeAttributeNames.CALLER_PHONE_NUMBER,
-		// "0102030105");
-		// e.addAttribute(ExchangeAttributeNames.RECIPIENT_PHONE_NUMBER,
-		// "0203040506");
-		// e
-		// .addAttribute(ExchangeAttributeNames.DATE, new Date()
-		// .toLocaleString());
-		// e.addAttribute(ExchangeAttributeNames.CONNECTION_DURATION, "12.2");
-		// AutoCommutator.getInstance().sendEvent(e);
-		//
-		// /* transfert service test */
-		//
-		// // create a transfert
-		// Event e2 = new Event(EventType.CREATE_TRANSFER);
-		// e2.addAttribute(ExchangeAttributeNames.CALLER_PHONE_NUMBER,
-		// "0203040506");
-		// e2.addAttribute(ExchangeAttributeNames.RECIPIENT_PHONE_NUMBER,
-		// "0304050607");
-		// AutoCommutator.getInstance().sendEvent(e2);
-		//
-		// // test transfer
-		// e = new Event(EventType.CALL_TRANSFER_REQUEST);
-		// e
-		// .addAttribute(ExchangeAttributeNames.CALLER_PHONE_NUMBER,
-		// "0102030105");
-		// e.addAttribute(ExchangeAttributeNames.RECIPIENT_PHONE_NUMBER,
-		// "0203040506");
-		// AutoCommutator.getInstance().sendEvent(e);
-		// // remove
-		// e = new Event(EventType.REMOVE_TRANSFER);
-		// e.addAttribute(ExchangeAttributeNames.PHONE_NUMBER, "0203040506");
-		// AutoCommutator.getInstance().sendEvent(e);
-		//
-		// // test transfer
-		// e = new Event(EventType.CALL_TRANSFER_REQUEST);
-		// e
-		// .addAttribute(ExchangeAttributeNames.CALLER_PHONE_NUMBER,
-		// "0102030105");
-		// e.addAttribute(ExchangeAttributeNames.RECIPIENT_PHONE_NUMBER,
-		// "0203040506");
-		// AutoCommutator.getInstance().sendEvent(e);
-
+		
 		System.out.println("Actives Connections : "
 				+ commutator.getActiveConnections());
 
