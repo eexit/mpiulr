3e7111766660f67ff5fde66cc1bb9f677f5d465f
Elian ORIOU <eoriou@gmail.com>
2011-03-06 21:53:01 +0000

    Scenario Traces Enhancement !

--- tests/fr/univ/lr/mpi/ScenarioExample.java
+++ tests/fr/univ/lr/mpi/ScenarioExample.java
@@ -1,8 +1,5 @@
 package fr.univ.lr.mpi;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import fr.univ.lr.mpi.commutator.impl.AutoCommutator;
 import fr.univ.lr.mpi.commutator.impl.Concentrator;
 import fr.univ.lr.mpi.lines.impl.Line;
@@ -41,7 +38,7 @@ public class ScenarioExample {
 		l1.pickUp();
 		l1.dialTo(l2.getPhoneNumber());
 		Thread.sleep(10000);
-		l1.sendMessage("Message à déposer sur répondeur");
+		l1.sendMessage("Message a deposer sur repondeur");
 		l1.hangUp();
 		
 		System.out.println("\n\nCase where Line 2 answers to Line 1 call, hangs up and recovers talk with Line 1"
@@ -55,26 +52,45 @@ public class ScenarioExample {
 		l1.sendMessage("Bien bien...");
 		l2.hangUp();
 		l2.pickUp();
-		l2.sendMessage("Désolé, j'ai fait une fausse manip'");
+		l2.sendMessage("Desole, j'ai fait une fausse manip'");
 		l1.sendMessage("Pas de soucis...");
 		l2.hangUp();
 		Thread.sleep(4000);
 		
 		System.out.println("\n\nCase where Line 1 hangs up on Line 2");
 		System.out.println("L1 pn: " + l1.getPhoneNumber());
+		
+		//FIXME Bizarre behavior !
+		concentrator.unregisterLine("0111111111");
+		concentrator.unregisterLine("0222222222");
+		l1 = new Line("0111111111");
+		l2 = new Line("0222222222");
+		concentrator.registerLine(l1);
+		concentrator.registerLine(l2);
+		
 		l1.pickUp();
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 		l1.dialTo(l2.getPhoneNumber());
+		Thread.sleep(2000);
 		l2.pickUp();
+		Thread.sleep(2000);
 		l1.sendMessage("Raccroche pas au nez !");
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 		l1.hangUp();
 		Thread.sleep(1000);
 		
+
+		//FIXME Bizarre behavior !
+		concentrator.unregisterLine("0222222222");
+		l2 = new Line("0222222222");
+		concentrator.registerLine(l2);
+		
 		System.out.println("\n\nCase where Line 2 recovers his answering machine messages");
+		Thread.sleep(2000);
 		l2.pickUp();
+		Thread.sleep(2000);
 		l2.dialTo(AnsweringService.ANSWERING_MACHINE_PHONE_NUMBER);
-		Thread.sleep(1000);
+		Thread.sleep(5000);
 		l2.hangUp();
 		
 		System.out.println("\nSimulation DONE.");
